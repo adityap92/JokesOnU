33976609a4651cff43bb97cd0d1fee22e3c3986a
aditya <adityap92@gmail.com>
2017-07-04 00:51:52 -0700

    adding more Jokes

--- javaJokes/src/main/java/com/example/Jokes.java
+++ javaJokes/src/main/java/com/example/Jokes.java
@@ -1,8 +1,28 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Jokes {
 
+    ArrayList<String> jokesList;
+    Random r;
+    int random;
+
+    public Jokes(){
+        r = new Random();
+
+        jokesList = new ArrayList<String>();
+        jokesList.add("Yo momma");
+        jokesList.add("The midget fortune teller who kills his customers is a small medium at large.");
+        jokesList.add("What does a nosey pepper do? Get jalapeño business.");
+        jokesList.add("If you want to catch a squirrel just climb a tree and act like a nut.");
+        jokesList.add("You kill vegetarian vampires with a steak to the heart.");
+    }
+
     public String tellJoke(){
-        return "Yo momma";
+
+        random = r.nextInt(jokesList.size());
+        return jokesList.get(random);
     }
 }
