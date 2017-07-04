a59ff84bac70f3e17aa0f9107aa895d60c6eb509
aditya <adityap92@gmail.com>
2017-07-04 00:16:29 -0700

    moved MainActivityFragment to common package for both free and paid flavors for equal access

--- app/src/main/java/com/jokesonu/MainActivity.java
+++ app/src/main/java/com/jokesonu/MainActivity.java
@@ -1,13 +1,10 @@
-package com.jokesonu.free;
+package com.jokesonu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import com.jokesonu.EndpointsAsyncTask;
-import com.jokesonu.R;
 
 
 public class MainActivity extends AppCompatActivity {
